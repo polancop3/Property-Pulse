43971300996f3177e30319608b81c1722a216c24
Pedro Polanco <pedropolanco@pedros-mbp.mynetworksettings.com>
2024-04-07 01:32:54 -0400

    models created and data is fetch from db

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,9 +1,13 @@
 import connectDB from "@/config/database";
+import Property from "@/models/Property";
 
+//GET /api/properties
 export const GET = async (request) => {
     try {
         await connectDB();
-        return new Response(JSON.stringify({message:'hellow world'}), { status: 200 })
+        const properties = await Property.find({});
+        console.log(properties)
+        return new Response(JSON.stringify(properties), { status: 200 })
     } catch (error) {
         console.log(error);
         new Response('something went wrong', { status: 500 })
